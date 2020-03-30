7f8358a14e44c5e15fb14c95066c5c935a63dfc7
M.Yasini <yasinimahdi98@gmail.com>
2020-03-30 12:18:13 +0430

    add i18n to index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import i18n from './i18n/i18n';
+import { I18nextProvider } from 'react-i18next';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <I18nextProvider i18n={i18n}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </I18nextProvider>,
   document.getElementById('root')
 );
 
